dd35fa994e0e864e2ae51674c74d9f128b3bf7c9
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-28 23:11:33 +0300

    fix:fix config.

--- src/main/java/com/springsecurityoauth2/config/SecurityConfig.java
+++ src/main/java/com/springsecurityoauth2/config/SecurityConfig.java
@@ -4,13 +4,11 @@ import com.springsecurityoauth2.security.CustomAuthenticationFailureHandler;
 import com.springsecurityoauth2.service.OAuth2UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
@@ -33,19 +31,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/", "/login", "/error", "/webjars/**").permitAll() // Разрешаем доступ к этим маршрутам всем
                         .requestMatchers("/h2-console/*").permitAll()
+                        .requestMatchers("/login").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN") // Только для администраторов
                         .anyRequest().authenticated() // Все остальные запросы требуют аутентификации
                 )
-                .exceptionHandling(exceptionHandling -> exceptionHandling
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)) // Обработка ошибок аутентификации
-                )
                 .oauth2Login(oauth2Login -> oauth2Login
-                        .loginPage("/") // Указываем страницу для входа
                         .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
                                 .userService(oAuth2UserService))
-                        .failureHandler(customAuthenticationFailureHandler)
                         .defaultSuccessUrl("/user")
                 )
                 .logout(logout -> logout
